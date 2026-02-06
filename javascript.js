35f9701d422275bf4ad60053dbef3af40bde20f0
ademankind <59447767+ademankind@users.noreply.github.com>
2026-02-06 14:10:54 +0100

    Change player input method to buttons
    add buttons for player choice to index.html
    
    add eventlistener for buttons to javascript.js
    
    add handleScore function to deal with result of
    playRound
    
    add DOM method to display score

--- javascript.js
+++ javascript.js
@@ -1,16 +1,13 @@
 
-let r = "Rock";
-let p = "Paper";
-let s = "Scissors";
+let r = "rock";
+let p = "paper";
+let s = "scissors";
 
-/*initialize score to 0*/
-let humanScore = 0;
-let computerScore = 0;
-
-
+let scorePlayer = 0;
+let scoreComputer = 0;
+let draws = 0;
 
 /*Generate random choice for Computer*/
-
 function getComputerChoice(){ 
 
  let x = Math.floor(Math.random()*60);
@@ -31,102 +28,87 @@ function getComputerChoice(){
 }
 
 
-/*Get input from human*/
 
-function getHumanChoice(){
-    let input = prompt("Play 5 Rounds!! Enter rock, paper or scissors");
-    let string = input.toLowerCase();
-    if (string === "rock"){
-        return r
-    }
-    else if (string === "paper"){
-        return p
-    }
-     else if (string === "scissors"){
-        return s
-    }
 
-}
 
 /*Play one round and increase score of winner*/
 
 function playRound(humanChoice, computerChoice){
 let x = humanChoice;
-let y = computerChoice;
+let y = getComputerChoice();
+
 
 if (x==r & y==p){
-     console.log("You Lose, Paper beats Rock")
-        computerScore++;
+    console.log("You Lose, Paper beats Rock")
+     handleScore('computerWin');
 }
 else if(x==r & y==s){ 
     console.log("You Win, Rock beats Scissors")
-    humanScore++;
+     handleScore('playerWin');
 
 }
 else if(x==s & y==p){ 
     console.log("You Win, Scissors beats Paper ")
-    humanScore++;
+     handleScore('playerWin');
 
 }else if(x==s & y==r){ 
     console.log("You Lose, Rock beats Scissors")
-     computerScore++;
+     handleScore('computerWin' );
 
 }
 else if(x==p & y==r){ 
     console.log("You Win, Paper beats Rock")
-    humanScore++;
+     handleScore('playerWin' );
 
 }
 else if(x==p & y==s){ 
     console.log("You Lose, Scissors beats Paper")
-    computerScore++;
+     handleScore('computerWin' );
 
 }
 
 else{
     console.log("It is a draw, !!")
+     handleScore('draw' );
 }
 
+
 }
 
-   
 
 
+function displayScore()
+{
+alert("  Player score: " + scorePlayer  + "    Computer score: " + scoreComputer + "    Draws: " + draws);
+}
 
-
-/*Play 5 rounds */
-function playGame(){
-
-   
-
-    for(let i = 0; i < 5; i++) {
-  
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("Human score is ", +humanScore+"  " + "Computer score is " + computerScore);
-     }
-
-    if( humanScore> computerScore){
-        alert("You Win!!");
-    }else if(computerScore>humanScore){
-        alert("You Lose :(");
-    }else{
-        alert("It's a draw");
+function handleScore(result) {
+    if (result === 'playerWin') {
+        scorePlayer++;
+    } else if (result === 'computerWin') {
+        scoreComputer++;
+    } else if (result === 'draw') {
+        draws++;
     }
 
-/*Set score to 0 after 5 rounds*/
-humanScore=0;
-computerScore=0;
+    // Display results
+    const resultsContainer = document.getElementById('results-container');
+    resultsContainer.textContent = "  Player: " + scorePlayer + "    Computer: " + scoreComputer + "   Draws: " + draws;
 
 }
 
-/*call the function*/
-playGame();
 
 
+// 1. Select the buttons
+const btn_r = document.getElementById('rock');
+const btn_p = document.getElementById('paper');
+const btn_s = document.getElementById('scissors');
+ 
 
 
-
+// 2. Add event listeners
+btn_r.addEventListener('click', () => playRound('rock'));
+btn_p.addEventListener('click', () => playRound('paper'));
+btn_s.addEventListener('click', () => playRound('scissors'));
 
 
